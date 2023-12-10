de277161fc39f20f68fb0ded62369353f83322cd
mbodell <mbodell@gmail.com>
2023-12-09 17:43:27 -0800

    Solved part1 of day 4

--- day04/index.js
+++ day04/index.js
@@ -7,7 +7,17 @@ let filename = process.argv.slice(2)[0] || 'input.txt';
 
 let answer = 0;
 
+let cards = [];
+
 eachLine(filename, function(line) {
+  let card = line.split(":")[1].trim();
+  cards.push(card.split(" | ").map((s)=>s.trim().split(" ").filter((f) => /\d+/.test(f)).map((n)=>parseInt(n))));
 }).then(function(err) {
+  for(let k=0;k<cards.length;k++) {
+    let num = cards[k][0].filter(value => cards[k][1].includes(value)).length;
+    if(num>0) {
+      answer += 2**(num-1);
+    }
+  }
   console.log(answer);
 });
