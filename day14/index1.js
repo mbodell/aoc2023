44cec12ec8a1e5482ee29e8b700a306eca588a6e
mbodell <mbodell@gmail.com>
2023-12-19 16:28:48 -0800

    Solved part 2 in clunky way

--- day14/index1.js
+++ day14/index1.js
@@ -10,6 +10,7 @@ let answer = 0;
 let floor = [];
 let sequence = [];
 let cycles = 1000000000;
+let temp = 20000;
 let weights = [];
 
 eachLine(filename, function(line) {
@@ -22,7 +23,7 @@ eachLine(filename, function(line) {
   let first = -1;
   let last = -1;
   let seq = 0;
-  for(let s=0;cycle<3 && s<cycles;s++) {
+  for(let s=0;cycle<3 && s<temp;s++) {
     // go North
     tilt = [];
     for(let y=0;y<rec.length;y++) {
@@ -114,26 +115,85 @@ eachLine(filename, function(line) {
     let sc = score(tilt);
     for(let k=0;k<weights.length;k++) {
       if(sc === weights[k]) {
-        if(compare(tilt,sequence[k])) {
+        if(compare(tilt,sequence[k],seq)) {
           if(cycle===0) {
             first = k;
             last = seq;
           }
           console.log(`We have found a cycle with weight ${sc} from ${first} to ${last}`);
           cycle++;
+        } else {
+          /*
+          if(seq>10000) {
+            console.log(`Despite ${sc} and ${weights[k]} being equal this was not a match on ${seq} and ${k}`);
+          }*/
         }
       }
     }
     weights.push(sc);
     if(s>0&&s%1000===0) {
       console.log(weights);
+      console.log(weights.slice(-100));
     }
   }
   console.log(weights);
-  let cycleLen = last - first;
-  cycles -= first;
-  let rem = cycles % cycleLen;
-  answer = weights[first + rem -1];
+  if(first>0) {
+    let cycleLen = last - first;
+    cycles -= first;
+    let rem = cycles % cycleLen;
+    answer = weights[first + rem -1];
+  } else {
+    let n14 = [];
+    let n03 = [];
+    let n85 = [];
+    let n71 = [];
+    let n62 = [];
+    let n61 = [];
+    let n81 = [];
+    let n01 = [];
+    let n20 = [];
+    for(let i=0;i<weights.length;i++) {
+      switch(weights[i]) {
+        case 96014:
+          n14.push(i); 
+          break;
+        case 96003:
+          n03.push(i);
+          break;
+        case 95985:
+          n85.push(i);
+          break;
+        case 95971:
+          n71.push(i);
+          break;
+        case 95962:
+          n62.push(i);
+          break;
+        case 95961:
+          n61.push(i);
+          break;
+        case 95981:
+          n81.push(i);
+          break;
+        case 96001:
+          n01.push(i);
+          break;
+        case 96020:
+          n20.push(i);
+          break;
+      }
+    }
+    console.log(n81);
+    console.log(n01);
+    console.log(n20);
+
+    let fir = n81[10];
+     
+    let cycleLen = 9;
+    cycles -= fir;
+    let rem = cycles % cycleLen;
+    answer = weights[fir + rem -1];
+  }
 
   console.log(answer);
 });
@@ -146,12 +206,16 @@ function score(tilt) {
   }
   return a;
 }
-function compare(a,b) {
+function compare(a,b,s) {
   let ret = true;
   for(let y=0;ret && y<a.length;y++) {
     for(let x=0;ret && x<a[y].length;x++) {
       if(a[y][x]!==b[y][x]) {
         ret = false;
+        /*
+        if(s>10000) {
+          console.log(`with y=${y} and x=${x} we have a non match on ${a[y][x]} and ${b[y][x]}\n${a[y]}\n${b[y]}`);
+        }*/
       }
     }
   }
