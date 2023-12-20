4fdc1ed21222d0828baacf3842c04b943faf4f5b
mbodell <mbodell@gmail.com>
2023-12-19 16:37:37 -0800

    Solved part 1

--- day15/index.js
+++ day15/index.js
@@ -6,8 +6,21 @@ const eachLine = Promise.promisify(lineReader.eachLine);
 let filename = process.argv.slice(2)[0] || 'input.txt';
 
 let answer = 0;
+let commands = [];
 
 eachLine(filename, function(line) {
+  commands = line.split(',');
 }).then(function(err) {
+  answer = commands.map((c)=>hashStr(c)).reduce((a,b)=>a+b);
   console.log(answer);
 });
+
+function hashStr(str) {
+  let ret = 0;
+  for(let k=0;k<str.length;k++) {
+    ret += str.charCodeAt(k);
+    ret *= 17;
+    ret = ret % 256;
+  }
+  return ret;
+}
